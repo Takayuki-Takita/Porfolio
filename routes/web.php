9629c5d4e0a0a2ee98afc3bf4f6259303983bcd1
HiranoDaisuke <zzzz.xo@gmail.com>
2020-05-30 08:17:20 +0000

    PHP/Laravel 15 投稿したニュース一覧を表示しよう

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::post('news/create', 'Admin\NewsController@create');
     Route::get('news/delete', 'Admin\NewsController@delete');
     Route::get('news/update', 'Admin\NewsController@update');
+    Route::get('news', 'Admin\NewsController@index');
     
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
